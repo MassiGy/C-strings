ae6c01b22e246d8260c5fed9f79bf2aae716d4e4
MassiGy <ghernaoutmassi@gmail.com>
2022-10-20 10:49:16 +0200

    run make

---
+++ src/build/main.d
@@ -0,0 +1,2 @@
+build/main.o: main.c ../headers/main.h ../headers/./stringManipulation.h \
+ ../headers/././sys_headers.h
---
+++ src/build/stringManipulation.d
@@ -0,0 +1,2 @@
+build/stringManipulation.o: stringManipulation.c \
+ ../headers/stringManipulation.h ../headers/./sys_headers.h
